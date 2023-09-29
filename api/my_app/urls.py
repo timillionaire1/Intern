981ed6c0e3c9edbdac2b05ac5479139e875caac2
koffy <timiakinyem@gmail.com>
2023-09-29 06:30:59 +0100

    second update

--- api/my_app/urls.py
+++ api/my_app/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    # path('home/', views.home),
-    path('', views.home),
+    path('home/', views.home),
+    # path('', views.home),
 ]
--- api/my_app/views.py
+++ api/my_app/views.py
@@ -28,3 +28,16 @@ def home(request):
             'form':details
         }
         return render(request, 'text.html', context)
+
+
+class SnippetList(APIView):
+    def get(self, request, format=None):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response (serializer.data)
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
